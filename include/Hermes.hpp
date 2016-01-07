bebfb560d68e66e575e1c07c3824e99a50e39a6b
Tommy <thomas.milox@epitech.eu>
2016-01-07 20:58:22 +0100

    [protobuf] 		- async_send tested and functional. 		- typedef callback in order to be able to pass a lambda as parameter.
    [tests]
    		- test_protobuf async_send implemented.

--- include/Hermes.hpp
+++ include/Hermes.hpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <algorithm>
+#include <functional>
 
 #include "google/protobuf/message.h"
 #include "asio.hpp"
@@ -27,14 +28,15 @@ std::size_t send(const std::string& host, const std::string& port,
   asio::io_context io_service;
 
   tcp::resolver resolver(io_service);
-  tcp::resolver::query query(host, port);
   tcp::socket socket(io_service);
-  auto endpoint = resolver.resolve(query);
+  auto endpoint = resolver.resolve(tcp::resolver::query(host, port));
 
   message->SerializeToString(&serialized);
   std::strcpy(buffer, serialized.c_str());
   asio::connect(socket, endpoint);
   asio::write(socket, asio::buffer(buffer, serialized.size()));
+  socket.close();
+  io_service.run();
   return serialized.size();
 }
 
@@ -48,6 +50,7 @@ std::shared_ptr<T> receive(const std::string& port) {
   tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), std::stoi(port)));
   acceptor.accept(socket);
   socket.read_some(asio::buffer(buffer), error);
+  socket.close();
 
   if (error == asio::error::eof)
     return nullptr;
@@ -57,8 +60,48 @@ std::shared_ptr<T> receive(const std::string& port) {
   auto response = buffer;
   auto result = std::make_shared<T>();
   result->ParseFromString(response);
+  io_service.run();
   return result;
 }
 
+typedef std::function<void()> callback;
+template <typename T>
+void async_send(const std::string& host, const std::string& port,
+                std::shared_ptr<T> message, const callback& handler =
+                nullptr) {
+  std::string serialized;
+  asio::io_context io_service;
+
+  tcp::socket socket(io_service);
+  tcp::resolver resolver(io_service);
+  message->SerializeToString(&serialized);
+  auto endpoint = resolver.resolve(tcp::resolver::query(host, port));
+  socket.async_connect(endpoint->endpoint(),
+                       [&](const asio::error_code& error) {
+
+    char buffer[2048] = {0};
+
+    if (error) {
+      socket.close();
+      return;
+    }
+
+    std::strcpy(buffer, serialized.c_str());
+    asio::async_write(socket, asio::buffer(buffer, serialized.size()),
+                      [&](const asio::error_code& error, std::size_t bytes) {
+
+      if (not bytes || error) {
+        socket.close();
+        return;
+      }
+
+      if (handler) handler();
+
+    });
+
+  });
+  io_service.run();
 }
-}
+
+}  // namespace protobuf
+}  // namespace Hermes
--- tests/protobuff.cpp
+++ tests/protobuff.cpp
@@ -13,6 +13,7 @@ void send_routine(std::shared_ptr<com::Message> message) {
   assert(size != 0);
 }
 
+
 void receive_routine() {
   auto test = protobuf::receive<com::Message>("1234");
   assert(test->name() == "Tommy");
@@ -22,9 +23,13 @@ void receive_routine() {
   assert(test->msg() == "Hope it works!");
 }
 
-void test_protobuf_synchronous_operations()
-{
-  std::string serialized;
+void async_send_routine(std::shared_ptr<com::Message> message) {
+  protobuf::async_send<com::Message>("127.0.0.1", "8080", message, [&](){
+      assert(message);
+  });
+}
+
+void test_protobuf_synchronous_operations() {
   auto protobuf_to_send = std::make_shared<com::Message>();
 
   protobuf_to_send->set_name("Tommy");
@@ -34,12 +39,22 @@ void test_protobuf_synchronous_operations()
   protobuf_to_send->set_msg("Hope it works!");
 
   std::thread thread_receive(receive_routine);
-  sleep(1);
   std::thread thread_send(send_routine, protobuf_to_send);
   thread_send.join();
   thread_receive.join();
 }
 
 void test_protobuf_asynchronous_operations() {
-  
+  auto protobuf_to_send = std::make_shared<com::Message>();
+
+  protobuf_to_send->set_name("1");
+  protobuf_to_send->set_object("2");
+  protobuf_to_send->set_from("3");
+  protobuf_to_send->set_to("4");
+  protobuf_to_send->set_msg("5");
+
+  std::thread thread_receive([](){ protobuf::receive<com::Message>("8080"); });
+  std::thread thread_send(async_send_routine, protobuf_to_send);
+  thread_send.join();
+  thread_receive.join();
 }
